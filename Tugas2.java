85872f4d71e85dc1616f6ee2cd5077daad301238
sombalsambel <sombalsambel@hotmail.com>
2017-02-27 09:29:22 +0700

    Add files via upload

---
+++ Tugas2.java
@@ -0,0 +1,10 @@
+public class Tugas2{
+
+     public static void main(String []args){
+         String nama = "Fahmi" ;
+         String kota = "Jepara" ;
+         String prodi = "Tif" ;
+         String fakultas = "Saintek";
+        System.out.println("Nama :"  + nama +  "Kota :" + kota +  "Prodi :" + prodi +  "Fakultas : " + fakultas);
+     }
+}
