58772569b203d6e6d8e4e3395954b882e415bc09
sombalsambel <sombalsambel@hotmail.com>
2017-02-27 10:00:35 +0700

    Add files via upload

---
+++ Tugas3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Tugas3{
+
+     public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("masukkan angka : ");
+        byte number = scanner.nextByte();
+        System.out.println("yang anda masukkan adalah angka" + number);
+     }
+}
+Tugas 3
+import java.util.Scanner;
+public class tugas3{
+
+     public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("masukkan angka : ");
+        byte number = scanner.nextByte();
+        System.out.println("yang anda masukkan adalah angka" + number);
+     }
+}
